636b18594ca0d7bde5fa7354ab05b63cf72c88b9
ssura <svensura@germanscreens.de>
2019-01-12 17:39:39 +0100

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose.js');
+const {Todo} = require('./../server/models/todo.js');
+const {User} = require('./../server/models/user.js');
+
+Todo.findByIdAndDelete('5c3a06e0c1d5ee741beaaa6b').then((result) => {
+    console.log(result);
+})
+
+Todo.findOneAndDelete({id_: '5c3a06e0c1d5ee741beaaa6b'}).then((result) => {
+    console.log(result);
+})
--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
-})
+});
 
 app.get('/todos/:id', (req, res) => {
  var id = req.params.id;
@@ -47,8 +47,20 @@ app.get('/todos/:id', (req, res) => {
     }
     res.send({todo});
     }).catch((e) => res.status(400).send());
-})
+});
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+       return res.status(404).send();
+    }
+    Todo.findByIdAndDelete(id).then((todo) => {
+       if (!todo) {
+           return res.status(404).send();
+       }
+       res.status(200).send({todo});
+       }).catch((e) => res.status(400).send());
+   });
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
