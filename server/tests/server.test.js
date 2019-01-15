7f5875e727478ae223cf647c2f92faf7b1fd2f1c
ssura <svensura@germanscreens.de>
2019-01-15 16:48:56 +0100

    add PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -85,8 +85,9 @@ describe('GET /todos/:id', () => {
     });
 
     it('should return 404 if todo not found', (done) => {
-         request(app)
-        .get(`/todos/${new ObjectID().toHexString()}`)
+        var valObjId = new ObjectID().toHexString();
+        request(app)
+        .get(`/todos/${valObjId}`)
         .expect(404)
         .end(done);
     });
@@ -100,4 +101,46 @@ describe('GET /todos/:id', () => {
     });
 
 
+});
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+      var hexId = todos[1]._id.toHexString();
+  
+      request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo._id).toBe(hexId);
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+  
+          Todo.findById(hexId).then((todo) => {
+            expect(todo).toBeNull;
+            done();
+          }).catch((e) => done(e));
+        });
+    });
+
+     it('should return 404 if todo not found', (done) => {
+        var valObjId = new ObjectID().toHexString();
+        request(app)
+        .delete(`/todos/${valObjId}`)
+        .expect(404)
+        .end(done);
+    });
+
+    
+    it('should return 404 if object id is invalid', (done) => {
+        var nonObjId = "123";
+        request(app)
+        .delete(`/todos/${nonObjId}`)
+        .expect(404)
+        .end(done);
+    });
+
+ 
 });
